105fccab3742301fb480f341f423d26388fd2d6c
charrlesss <charlespalencia21@gmail.com>
2024-09-13 14:23:57 +0800

    f

--- index.js
+++ index.js
@@ -2,45 +2,50 @@ const express = require("express");
 const app = express();
 const mysql = require("mysql2");
 
-
 const _PORT = 9999;
 
 const db = mysql.createConnection({
   host: "localhost",
-  user: "root",
-  password: "",
+  user: "upward_user",
+  password: "upward123",
   database: "upward",
 });
-// async function executeQuery(sql, callback) {
-//     if (db.state === 'disconnected') {
-//         db.connect(err => {
-//             if (err) {
-//                 console.log(err)
-//                 return callback(err, null);
-//             }
-//             db.query(sql, callback);
-//         });
-//     } else {
-//          db.query(sql, callback);
-//     }
-// }
+async function executeQuery(sql, callback) {
+  if (db.state === "disconnected") {
+    db.connect((err) => {
+      if (err) {
+        console.log(err);
+        return callback(err, null);
+      }
+      db.query(sql, callback);
+    });
+  } else {
+    db.query(sql, callback);
+  }
+}
 
-app.get('/users', async (req,res)=>{
-   try {
-   
+app.get("/users", async (req, res) => {
+  try {
+    executeQuery(`select * from users`, (error, rows) => {
+      if (error) {
+        res.send({
+          message: error.message,
+        });
+      }
+      res.send({
+        message: "manok na pula",
+        rows,
+      });
+    });
+    
+  } catch (error) {
+    console.log(error.message);
     res.send({
-        message:"manok na pula"
-    })
-   } catch (error) {
-    console.log(error.message)
-    res.send({
-        message:error.message
-    })
-   }
-})
-
+      message: error.message,
+    });
+  }
+});
 
 app.listen(_PORT, () => {
   console.log(`listen on http://localhost:${_PORT}/`);
 });
-
